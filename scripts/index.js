935c179388d73331f2efa32b1d60f8d17fe32e18
Aleksey Tishenkov <tishenkov@gmail.com>
2023-03-24 03:09:04 +0400

    feat: implement tabs and dropdown menu for Bikes section

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,15 @@ import * as slider from "./slider.js";
 import * as darkmode from "./darkmode.js";
 
 // --- Opening/closing mobile nav menu ---
-const navMenuLinks = document.querySelectorAll(".header__link");
-const navButton = document.querySelector(".hamburger-menu");
-const navButtonInput = navButton.querySelector("input");
+
+const navMenuLinks = document.querySelectorAll(".header__link"),
+  navButton = document.querySelector(".hamburger-menu"),
+  navButtonInput = navButton.querySelector("input"),
+  themeSwitcher = document.querySelector(".theme-switcher");
 
 navButton.addEventListener("click", () => {
   navButton.setAttribute("data-open", navButtonInput.checked);
+  themeSwitcher.setAttribute("data-open", navButtonInput.checked);
 });
 
 // Closing mobile nav menu when clicking a nav link
@@ -15,7 +18,35 @@ navMenuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     navButtonInput.checked = !navButtonInput.checked;
     navButton.setAttribute("data-open", navButtonInput.checked);
+    themeSwitcher.setAttribute("data-open", navButtonInput.checked);
   });
+});
+
+// ---
+
+// --- Linking radio buttons and dropdown menu
+
+const dropdownMenu = document.querySelector(".bikes__dropdown");
+const tabs = document.querySelectorAll(".tab");
+
+function setRadioBtn() {
+  const radioBtn = document.querySelector(`[data-id="${dropdownMenu.value}"]`);
+  radioBtn.checked = true;
+}
+
+function setList(evt) {
+  const selectedValue = evt.textContent;
+  dropdownMenu.querySelector(`[value=${selectedValue}]`).selected = true;
+}
+
+tabs.forEach((tab) => {
+  tab.addEventListener("mousedown", (evt) => {
+    setList(evt.target);
+  });
+});
+
+dropdownMenu.addEventListener("change", () => {
+  setRadioBtn();
 });
 
 // ---
