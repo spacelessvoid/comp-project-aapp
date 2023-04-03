2dd947670430b9233b10668fbd6ac06b321da01d
Aleksey Tishenkov <tishenkov@gmail.com>
2023-04-03 20:07:31 +0400

    refactor: simplify slider code

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,25 +5,36 @@
 const surfaceTitleElements = document.querySelectorAll(".surface__title");
 const surfaceTextElements = document.querySelectorAll(".surface__text");
 const surfaceImageElements = document.querySelectorAll(".surface__image");
+const imagesArrLength = surfaceImageElements.length;
 const surfaceTypeElement = document.querySelector(".surface__type");
 const surfaceButtonLeft = document.querySelector(".surface__button_left");
 const surfaceButtonRight = document.querySelector(".surface__button_right");
-const bikesSurfaceTypes = document.querySelectorAll(".bikes__type");
-const bikesDropdownItems = document.querySelectorAll(
-  ".bikes__dropdown-item"
-);
-const bikesCategoryElements = document.querySelectorAll(".bikes__category");
+
+const shiftedElements = [surfaceTitleElements, surfaceTextElements];
 
 let n = 0;
 
 function handleSurfaceType() {
   surfaceTypeElement.className = "surface__type";
-  surfaceTypeElement.classList.add("surface__type_image_" + (n + 1));
+  surfaceTypeElement.classList.add(`surface__type_image_${n + 1}`);
 }
 
-function scrollButtonRight() {
-  if (n === surfaceImageElements.length - 1) {
-    for (let i = 0; i < surfaceImageElements.length; i++) {
+// Function for setting the display value of text elements (<h2> & <p> tags) related to the selected image / surface type
+// n2 is the index of selected image / surface type
+function setDisplayProp(
+  { prop1 = "none", n1 },
+  { prop2 = "block", n2 },
+  elements = [...shiftedElements]
+) {
+  elements.forEach((element) => {
+    element[n1].style.display = prop1;
+    element[n2].style.display = prop2;
+  });
+}
+
+function shiftRight() {
+  if (n === imagesArrLength - 1) {
+    for (let i = 0; i < imagesArrLength; i++) {
       surfaceImageElements[i].style.order = 0;
     }
 
@@ -31,59 +42,30 @@ function scrollButtonRight() {
 
     handleSurfaceType();
 
-    surfaceTitleElements[surfaceTitleElements.length - 1].style.display = "none";
-    surfaceTitleElements[n].style.display = "block";
-    surfaceTextElements[surfaceTextElements.length - 1].style.display = "none";
-    surfaceTextElements[n].style.display = "block";
-
-    // bikesCategoryElement[bikesSurfaceTypes.length - 1].style.display = "none";
-    // bikesCategoryElement[n].style.display = "flex";
-
-    // bikesSurfaceTypes[bikesSurfaceTypes.length - 1].classList.add(
-    //   "bikes__type_inactive"
-    // );
-    // bikesSurfaceTypes[n].classList.remove("bikes__type_inactive");
+    setDisplayProp({ n1: imagesArrLength - 1 }, { n2: n });
   } else {
-    surfaceImageElements[n].style.order = n + surfaceImageElements.length;
+    surfaceImageElements[n].style.order = n + imagesArrLength;
 
     n++;
 
     handleSurfaceType();
 
-    surfaceTitleElements[n - 1].style.display = "none";
-    surfaceTitleElements[n].style.display = "block";
-    surfaceTextElements[n - 1].style.display = "none";
-    surfaceTextElements[n].style.display = "block";
-
-    // bikesCategoryElement[n - 1].style.display = "none";
-    // bikesCategoryElement[n].style.display = "flex";
-
-    // bikesSurfaceTypes[n - 1].classList.add("bikes__type_inactive");
-    // bikesSurfaceTypes[n].classList.remove("bikes__type_inactive");
+    setDisplayProp({ n1: n - 1 }, { n2: n });
   }
 }
 
-function scrollButtonLeft() {
+function shiftLeft() {
   if (n === 0) {
     // sets the conditions as if user scrolled to the last image
-    for (let i = 0; i < surfaceImageElements.length - 1; i++) {
-      surfaceImageElements[i].style.order = i + surfaceImageElements.length;
+    for (let i = 0; i < imagesArrLength - 1; i++) {
+      surfaceImageElements[i].style.order = i + imagesArrLength;
     }
 
-    n = surfaceImageElements.length - 1;
+    n = imagesArrLength - 1;
 
     handleSurfaceType();
 
-    surfaceTitleElements[0].style.display = "none";
-    surfaceTitleElements[n].style.display = "block";
-    surfaceTextElements[0].style.display = "none";
-    surfaceTextElements[n].style.display = "block";
-
-    // bikesCategoryElement[0].style.display = "none";
-    // bikesCategoryElement[n].style.display = "flex";
-
-    // bikesSurfaceTypes[0].classList.add("bikes__type_inactive");
-    // bikesSurfaceTypes[n].classList.remove("bikes__type_inactive");
+    setDisplayProp({ n1: 0 }, { n2: n });
   } else {
     n--;
 
@@ -91,40 +73,9 @@ function scrollButtonLeft() {
 
     handleSurfaceType();
 
-    surfaceTitleElements[n + 1].style.display = "none";
-    surfaceTitleElements[n].style.display = "block";
-    surfaceTextElements[n + 1].style.display = "none";
-    surfaceTextElements[n].style.display = "block";
-
-    // bikesCategoryElement[n + 1].style.display = "none";
-    // bikesCategoryElement[n].style.display = "flex";
-
-    // bikesSurfaceTypes[n + 1].classList.add("bikes__type_inactive");
-    // bikesSurfaceTypes[n].classList.remove("bikes__type_inactive");
+    setDisplayProp({ n1: n + 1 }, { n2: n });
   }
 }
 
-// function switchTypeMenu(e) {
-//   for (let i = 0; i < e.length; i++) {
-//     e[i].addEventListener("click", () => {
-//       if (i === n + 1 && (i === 1 || i === 2)) {
-//         scrollButtonRight();
-//         n = i;
-//       } else if (i === n + 2) {
-//         scrollButtonRight();
-//         scrollButtonRight();
-//       } else if (i === n - 2) {
-//         scrollButtonLeft();
-//         scrollButtonLeft();
-//       } else if (i === n - 1) {
-//         scrollButtonLeft();
-//       }
-//     });
-//   }
-// }
-
-surfaceButtonRight.addEventListener("click", scrollButtonRight);
-surfaceButtonLeft.addEventListener("click", scrollButtonLeft);
-
-// switchTypeMenu(bikesSurfaceTypes);
-// switchTypeMenu(bikesDropdownItems);
+surfaceButtonRight.addEventListener("click", shiftRight);
+surfaceButtonLeft.addEventListener("click", shiftLeft);
